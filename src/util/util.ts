a748397785af7586c9762e42412917e12825d6f0
Zulbil <alexkhang25@yahoo.fr>
2022-02-17 17:08:27 -0600

    adding delay before removing local files

--- src/util/util.ts
+++ src/util/util.ts
@@ -14,7 +14,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
       const photo = await Jimp.read(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
+      photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
@@ -33,7 +33,9 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
-  for (let file of files) {
-    fs.unlinkSync(file);
-  }
+  setTimeout(()=> {
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
+  }, 2000);
 }
